a6e33223add1c6aea8872d273724c5e990506a98
Bhushan kakade <kakadebhushan467@gmail.com>
2024-06-23 09:20:19 +0530

    create Delete PatientData RestApi

--- Hospital-Management-System/src/main/java/com/Hospital/Management/System/controller/PatientConroller.java
+++ Hospital-Management-System/src/main/java/com/Hospital/Management/System/controller/PatientConroller.java
@@ -2,6 +2,7 @@ package com.Hospital.Management.System.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Hospital.Management.System.entity.Patient;
 import com.Hospital.Management.System.repositorty.PatientRepository;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/api/vi")
 public class PatientConroller {
--- Hospital-Management-System/src/main/java/com/Hospital/Management/System/doclogin/controller/AppointmantController.java
+++ Hospital-Management-System/src/main/java/com/Hospital/Management/System/doclogin/controller/AppointmantController.java
@@ -1,8 +1,16 @@
 package com.Hospital.Management.System.doclogin.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.management.AttributeNotFoundException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,25 +18,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.Hospital.Management.System.doclogin.entity.Appointment;
 import com.Hospital.Management.System.doclogin.repository.AppointmentsRepository;
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/api/v2")
 public class AppointmantController {
 
-	private AppointmentsRepository appointmentsrepository;
+	private AppointmentsRepository appointmentsRepository;
 
 	public AppointmantController(AppointmentsRepository appointmentsrepository) {
 		super();
-		this.appointmentsrepository = appointmentsrepository;
+		this.appointmentsRepository = appointmentsrepository;
 	}
 	@PostMapping("/insert")
 	public Appointment createAppointment(@RequestBody Appointment appointment) {
-		return appointmentsrepository.save(appointment);
+		return appointmentsRepository.save(appointment);
 	}
 	
 	//get the data appointment
-	@GetMapping
+	@GetMapping("/appointments")
 	public List<Appointment>getAllAppointments(){
-		return appointmentsrepository.findAll();
+		return appointmentsRepository.findAll();
+	}
+	
+	//Delete patient rest Api
+	@DeleteMapping("/appointments/{id}")
+	public ResponseEntity<Map<String,Boolean>>deleteAppointment(@PathVariable long id) throws AttributeNotFoundException{
+		Appointment appointment=appointmentsRepository.findById(id).orElseThrow(()->new AttributeNotFoundException("Appointment not found with id"+id));
+	
+		appointmentsRepository.delete(appointment);
+		Map<String,Boolean>response=new HashMap<String,Boolean>();
+		response.put("Deleted",Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 }
