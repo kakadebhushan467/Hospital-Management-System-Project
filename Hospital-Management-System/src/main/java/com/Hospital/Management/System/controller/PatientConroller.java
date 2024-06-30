293a3e7c78e9e33e497d4a73b96c5b04809a2d2b
Bhushan kakade <kakadebhushan467@gmail.com>
2024-06-30 09:51:53 +0530

    Create Medicine RestApi GetMedicines,PutMedicines And DeleteMedicenes

--- Hospital-Management-System/src/main/java/com/Hospital/Management/System/controller/PatientConroller.java
+++ Hospital-Management-System/src/main/java/com/Hospital/Management/System/controller/PatientConroller.java
@@ -1,9 +1,16 @@
 package com.Hospital.Management.System.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.management.AttributeNotFoundException;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,15 +31,27 @@ public class PatientConroller {
 		super();
 		this.patientRepository = patientRepository;
 	}
-	@PostMapping("/insert")
+	@PostMapping("/patients")
 	public Patient createPatient(@RequestBody Patient patient) {
 		return patientRepository.save(patient);
 	}
 	
 	//Get the all data patient
-	@GetMapping
+	@GetMapping("/patients")
 	public List<Patient>getAllPatient(){
 		return patientRepository.findAll();
+		
+	}
+	
+	@DeleteMapping("/patients/{id}")
+	public ResponseEntity<Map<String,Boolean>>deletePatient(@PathVariable long id) throws AttributeNotFoundException{
+		
+	Patient patient=patientRepository.findById(id).orElseThrow(()->new AttributeNotFoundException("Patient not found with id:"+id));
+	patientRepository.delete(patient);
+	Map<String,Boolean>response=new HashMap<String,Boolean>();
+	response.put("Deleted", Boolean.TRUE);
+	return ResponseEntity.ok(response);
+	
 		
 	}
 }
